0418a5dc8f406f2ce7243763099031e98f07264e
songhee24 <akaratel61@gmail.com>
2023-09-11 12:41:49 +0600

    logic of async testing with await syntax

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-
-describe('React Core Test',  () => {
-  test('renders hello world element', () => {
+describe("React Core Test", () => {
+  test("renders hello world element", () => {
     render(<App />);
     const helloWorldElem = screen.getByText(/hello world/i);
-    const buttonElem = screen.getByRole('button');
+    const buttonElem = screen.getByRole("button");
     const inputElem = screen.getByPlaceholderText(/input value/i);
     expect(helloWorldElem).toBeInTheDocument();
     expect(buttonElem).toBeInTheDocument();
@@ -14,12 +13,12 @@ describe('React Core Test',  () => {
     // screen.debug()
   });
 
-  test('screen methods', () => {
+  test("screen methods", async () => {
     render(<App />);
     // const helloWorldElem = screen.queryByText(/hello world2/i)
     // expect(helloWorldElem).toBeNull()
     // screen.debug()
-    const helloWorldElem = screen.findByText(/hello world2/i)
-    expect(helloWorldElem).toBeNull()
+    const helloWorldElem = await screen.findByText(/data/i);
+    expect(helloWorldElem).toBeInTheDocument();
   });
-})
+});
