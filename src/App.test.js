48c3dd3b66c18166b2654eae51f31e93de7029ad
songhee24 <akaratel61@gmail.com>
2023-09-11 12:23:40 +0600

    another methods of reacttl

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,9 @@ import App from './App';
 test('renders hello world element', () => {
   render(<App />);
   const helloWorldElem = screen.getByText(/hello world/i);
+  const buttonElem = screen.getByRole('button');
+  const inputElem = screen.getByPlaceholderText(/input value/i);
   expect(helloWorldElem).toBeInTheDocument();
+  expect(buttonElem).toBeInTheDocument();
+  expect(inputElem).toBeInTheDocument();
 });
