f82fdeb9aff8c67fab03a595f56330a108d577fe
songhee24 <akaratel61@gmail.com>
2023-09-11 12:35:15 +0600

    groupping test and also define screen.query methods

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders hello world element', () => {
-  render(<App />);
-  const helloWorldElem = screen.getByText(/hello world/i);
-  const buttonElem = screen.getByRole('button');
-  const inputElem = screen.getByPlaceholderText(/input value/i);
-  expect(helloWorldElem).toBeInTheDocument();
-  expect(buttonElem).toBeInTheDocument();
-  expect(inputElem).toMatchSnapshot();
-  // screen.debug()
-});
+
+describe('React Core Test',  () => {
+  test('renders hello world element', () => {
+    render(<App />);
+    const helloWorldElem = screen.getByText(/hello world/i);
+    const buttonElem = screen.getByRole('button');
+    const inputElem = screen.getByPlaceholderText(/input value/i);
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(buttonElem).toBeInTheDocument();
+    expect(inputElem).toMatchSnapshot();
+    // screen.debug()
+  });
+
+  test('screen methods', () => {
+    render(<App />);
+    const helloWorldElem = screen.queryByText(/hello world2/i)
+    expect(helloWorldElem).toBeNull()
+    // screen.debug()
+  });
+})
