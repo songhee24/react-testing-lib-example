b40dd8aed80bf606f3748d0046d8ce3bfcd46c51
songhee24 <akaratel61@gmail.com>
2023-09-12 04:09:42 +0600

    pages for react router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
-import React, { useEffect } from "react";
-import Users from "./users/Users";
+import React from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
+// import Users from "./users/Users";
 
-function App() {
-  const [data, setData] = React.useState(null);
-  const [toggle, setToggle] = React.useState(false);
-  const [value, setValue] = React.useState("");
+// function App() {
+//   const [data, setData] = React.useState(null);
+//   const [toggle, setToggle] = React.useState(false);
+//   const [value, setValue] = React.useState("");
+//
+//   const toggleHandler = () => setToggle((prevState) => !prevState);
+//   const changeHandler = (e) => setValue(e.target.value);
+//
+//   React.useEffect(() => {
+//     setTimeout(() => {
+//       setData({});
+//     }, 500);
+//   }, []);
+//
+//   return (
+//     <div>
+//       <h1 data-testid="value-elem">{value}</h1>
+//       {toggle === true && <div data-testid="toggle-elem">toggle</div>}
+//       {data && <div style={{ color: "red" }}>data</div>}
+//       <h1>Hello World</h1>
+//       <button data-testid="toggle-btn" onClick={toggleHandler}>
+//         click me
+//       </button>
+//       <input
+//         value={value}
+//         onChange={changeHandler}
+//         type="text"
+//         placeholder="input value..."
+//       />
+//
+//       <Users />
+//     </div>
+//   );
+// }
 
-  const toggleHandler = () => setToggle((prevState) => !prevState);
-  const changeHandler = (e) => setValue(e.target.value);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setData({});
-    }, 500);
-  }, []);
-
+const App = () => {
   return (
-    <div>
-      <h1 data-testid="value-elem">{value}</h1>
-      {toggle === true && <div data-testid="toggle-elem">toggle</div>}
-      {data && <div style={{ color: "red" }}>data</div>}
-      <h1>Hello World</h1>
-      <button data-testid="toggle-btn" onClick={toggleHandler}>
-        click me
-      </button>
-      <input
-        value={value}
-        onChange={changeHandler}
-        type="text"
-        placeholder="input value..."
-      />
-
-      <Users />
-    </div>
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
