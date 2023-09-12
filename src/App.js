f588392d0cd7be94d766ba8e643a2b6455e22fe1
songhee24 <akaratel61@gmail.com>
2023-09-12 16:48:42 +0600

    using router components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import AboutPage from "./pages/AboutPage";
-import ErrorPage from "./pages/ErrorPage";
-import Users from "./users/Users";
-import UserDetailsPage from "./pages/UserDetailsPage";
+import { Link } from "react-router-dom";
+import AppRouter from "./router/AppRouter";
 // import Users from "./users/Users";
 
 // function App() {
@@ -54,13 +50,7 @@ const App = () => {
       <Link to="/users" data-testid="users-link">
         about
       </Link>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/users/:id" element={<UserDetailsPage />} />
-        <Route path="/*" element={<ErrorPage />} />
-      </Routes>
+      <AppRouter />
     </div>
   );
 };
