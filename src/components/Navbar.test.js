d91e5b847988fc2aab7de19daddc4d007974aab5
songhee24 <akaratel61@gmail.com>
2023-09-12 17:27:15 +0600

    separate test cases for better debaging

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -3,15 +3,23 @@ import { renderWithRouter } from "../tests/helpers/renderWithRouter";
 import Navbar from "./Navbar";
 
 describe("NAVBAR TEST", () => {
-  test("navbar router testing", () => {
+  test("navbar main link testing", () => {
     render(renderWithRouter(<Navbar />));
     const mainLink = screen.getByTestId("main-link");
-    const aboutLink = screen.getByTestId("about-link");
-    const usersLink = screen.getByTestId("users-link");
-    fireEvent.click(aboutLink);
-    expect(screen.getByTestId("about-page")).toBeInTheDocument();
     fireEvent.click(mainLink);
     expect(screen.getByTestId("main-page")).toBeInTheDocument();
+  });
+
+  test("navbar about testing", () => {
+    render(renderWithRouter(<Navbar />));
+    const aboutLink = screen.getByTestId("about-link");
+    fireEvent.click(aboutLink);
+    expect(screen.getByTestId("about-page")).toBeInTheDocument();
+  });
+
+  test("navbar users testing", () => {
+    render(renderWithRouter(<Navbar />));
+    const usersLink = screen.getByTestId("users-link");
     fireEvent.click(usersLink);
     expect(screen.getByTestId("users-page")).toBeInTheDocument();
   });
