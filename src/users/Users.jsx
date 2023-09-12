616671faabc285556cff85182e62b81f952f7db7
songhee24 <akaratel61@gmail.com>
2023-09-12 14:09:55 +0600

    loading some data via axios

--- src/users/Users.jsx
+++ src/users/Users.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 const Users = () => {
   const [users, setUsers] = React.useState([]);
+
+  const loadUsers = async () => {
+    const resp = await axios.get("https://jsonplaceholder.typicode.com/users");
+    setUsers(resp.data);
+  };
+
+  React.useEffect(() => {
+    loadUsers();
+  }, []);
 
   return (
     <div>
