8c5e742172f52a0864dafd5f17494542690e1c17
songhee24 <akaratel61@gmail.com>
2023-09-13 18:36:01 +0600

    combine reducer logic

---
+++ src/store/reducers/counterReducer.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterReducer = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement } = counterReducer.actions;
+
+export default counterReducer.reducer;
