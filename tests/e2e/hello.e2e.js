f7bcddd0c85f39fb596289d7acc0453b58a9bc44
songhee24 <akaratel61@gmail.com>
2023-09-14 13:57:51 +0600

    done with tests

--- tests/e2e/hello.e2e.js
+++ tests/e2e/hello.e2e.js
@@ -1,11 +1,17 @@
 const HelloPage = require("../pages/hello.page");
 const { expect } = require("@wdio/globals");
 describe("My Hello World application", () => {
-  it("test", async () => {
+  it("should toggle", async () => {
     await HelloPage.open();
     await HelloPage.toggleTitleWithInput("hello");
     await expect(HelloPage.helloTitle).toBeExisting();
     await HelloPage.toggleBtn.click();
+    await expect(HelloPage.helloTitle).not.toBeExisting();
+  });
+
+  it("should not toggle", async () => {
+    await HelloPage.open();
+    await HelloPage.toggleTitleWithInput("test");
     await expect(HelloPage.helloTitle).not.toBeExisting();
   });
 });
