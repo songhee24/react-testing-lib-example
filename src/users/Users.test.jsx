0bd8922dc9f5459a58bf7215a98905fb1a7ea169
songhee24 <akaratel61@gmail.com>
2023-10-27 16:26:38 +0600

    redirect to details page works but another test case broken

--- src/users/Users.test.jsx
+++ src/users/Users.test.jsx
@@ -1,7 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Users from "./Users";
 import axios from "axios";
-import userEvent from "@testing-library/user-event";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import UserDetailsPage from "../pages/UserDetailsPage";
+import React from "react";
 jest.mock("axios");
 describe("USERS TEST", () => {
   let response;
@@ -37,10 +39,17 @@ describe("USERS TEST", () => {
 
   test("redirect to details page test", async () => {
     axios.get.mockReturnValue(response);
-    render(<Users />);
+    render(
+      <MemoryRouter initialEntries={["/users"]}>
+        <Routes>
+          <Route path="/users" element={<Users />} />
+          <Route path="/users/:id" element={<UserDetailsPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
     const users = await screen.findAllByTestId("user-item");
     expect(users.length).toBe(3);
-    userEvent.click(users[0]);
+    fireEvent.click(users[0]);
     expect(screen.getByTestId("user-page")).toBeInTheDocument();
   });
 });
